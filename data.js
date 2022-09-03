5b26c53e9a02a6c5aa965766b45010c532b6a1fa
Shujaat09 <112425715+Shujaat09@users.noreply.github.com>
2022-09-03 12:12:15 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,99 @@
+
+ 1
+ 2
+ 3
+ 4
+ 5
+ 6
+ 7
+ 8
+ 9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+var firebaseConfig = {
+    apiKey: "AIzaSyDPA2ErRa_cECEixKP5z_fRhcJH-NmQU-A",
+    authDomain: "signuppage-61d08.firebaseapp.com",
+    databaseURL: "https://signuppage-61d08-default-rtdb.firebaseio.com",
+    projectId: "signuppage-61d08",
+    storageBucket: "signuppage-61d08.appspot.com",
+    messagingSenderId: "375117672256",
+    appId: "1:375117672256:web:27facf0382fdfac51cd4a5",
+    measurementId: "G-BE09K4JP2Y"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+// let's code 
+var datab  = firebase.database().ref('data');
+function UserRegister(){
+var email = document.getElementById('eemail').value;
+var password = document.getElementById('lpassword').value;
+firebase.auth().createUserWithEmailAndPassword(email,password).then(function(){
+    
+}).catch(function (error){
+    var errorcode = error.code;
+    var errormsg = error.message;
+});
+}
+const auth = firebase.auth();
+function SignIn(){
+    var email = document.getElementById('eemail').value;
+    var password = document.getElementById('lpassword').value;
+    const promise = auth.signInWithEmailAndPassword(email,password);
+    promise.catch( e => alert(e.msg));
+    window.open("https://www.google.com","_self");
+}
+document.getElementById('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    var userInfo = datab.push();
+    userInfo.set({
+        name: getId('fname'),
+        email : getId('eemail'),
+        password : getId('lpassword')
+    });
+    alert("Successfully Signed Up");
+    console.log("sent");
+    document.getElementById('form').reset();
+});
+function  getId(id){
+    return document.getElementById(id).value;
+}
